3d9401eef85004f7587d5162fdc3ec90f6a0c495
lauren brett <laurencareybrett@gmail.com>
2020-01-08 16:14:40 +0000

    7 tests

--- models/traveller.js
+++ models/traveller.js
@@ -10,7 +10,9 @@ Traveller.prototype.getJourneyStartLocations = function () {
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  return this.journeys.map((journey) => {
+    return journey.endLocation
+  })
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
--- specs/traveller_spec.js
+++ specs/traveller_spec.js
@@ -38,7 +38,7 @@ describe('Traveller', function() {
     assert.deepStrictEqual(traveller.getJourneyStartLocations(), expected);
   });
 
-  xit('should be able to get the journeys end locations', function() {
+  it('should be able to get the journeys end locations', function() {
     const expected = [
       journey1.endLocation,
       journey2.endLocation,
